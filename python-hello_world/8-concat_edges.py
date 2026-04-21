992d693aa26578d12a93e8ee31a940a24055838c
Lesliemugiraneza13 <l.mugiranez@alustudent.com>
2026-04-21 08:56:43 -0700

    Fix concat edges using string slicing

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[:6] + word[9:18] + word[20:29] + word[30:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[26:69] + str[0:6])
