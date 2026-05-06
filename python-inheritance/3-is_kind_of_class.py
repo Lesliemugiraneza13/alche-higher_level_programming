dbbe10f503a0b6e95031107fe724413557aa3a4a
Lesliemugiraneza13 <l.mugiranez@alustudent.com>
2026-05-06 07:43:48 -0700

    Fix failing checks for inheritance tasks

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """Module that checks if object is instance of or inherits from a class"""
 
 
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module that checks if object is instance of a class that inherited from specified class"""
+"""Module that checks if object inherits from specified class"""
 
 
 def inherits_from(obj, a_class):
-    """Returns True if obj is instance of a class that inherited from a_class"""
+    """Returns True if obj is instance of a class that inherited
+    from a_class but is not a_class itself"""
     return isinstance(obj, a_class) and type(obj) is not a_class
