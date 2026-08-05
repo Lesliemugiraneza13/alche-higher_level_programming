562f1b5bf88dd6805f662d726f18e24f80cf4ab5
Leslie Mugiraneza <l.mugiranez@alustudent.com>
2026-08-05 21:36:18 +0400

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
+
+const args = process.argv.slice(2);
 
 if (args.length < 2) {
   console.log(0);
 } else {
-  const sorted = args.sort((a, b) => b - a);
-  console.log(sorted[1]);
+  const numbers = args.map((arg) => parseInt(arg, 10));
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
 }
