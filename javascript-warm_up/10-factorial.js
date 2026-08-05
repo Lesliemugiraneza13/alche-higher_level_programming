5b5927bce778992612b1ac7c0c794f713f6bb027
Leslie Mugiraneza <l.mugiranez@alustudent.com>
2026-08-05 21:34:53 +0400

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 function factorial (n) {
-  if (isNaN(n) || n <= 1) {
+  if (Number.isNaN(n) || n <= 1) {
     return 1;
   }
   return n * factorial(n - 1);
 }
 
-const arg = parseInt(process.argv[2], 10);
-console.log(factorial(arg));
+const num = parseInt(process.argv[2], 10);
+
+console.log(factorial(num));
